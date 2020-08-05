dc99db9382ea510a89b3f5e0417d2a5da39ea3de
Anderson Silva <ali3ncode@gmail.com>
2020-08-04 21:57:54 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto Mídia Pack
+
+Este projeto está sendo desenvolvimento sobe meu dominio sobre total responsabilidade e nenhum custo, visando futuramente alianças e trabalhos com o dono da empresa ou recomendações do mesmo para amigos e diversos.
+
+## Sobre o Projeto
+
+Se trata de uma página de venda para um produto digital. Produto esse que se baseia em um pacote de arquivos editáveis, contendo imagens e videos para empresas e empreendedores buscando alavancar a marca ou produto.
+
+> Versão: 1.0
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- Primordial tags -->
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<!-- Title and Icon -->
+		<title></title>
+		<link rel="icon" href="favicon.png" type="image/png"/>
+
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="css/bootstrap.css"/>
+		<link rel="stylesheet" href="css/app.css"/>
+		<link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		
+		<!-- Header Section -->
+			<div id="header">
+				<!-- Here we start -->
+			</div>
+		<!-- Header Section: End -->
+
+		<!-- Body Section -->
+			<div id="body"></div>
+		<!-- Body Section: End -->
+
+		<!-- Footer Section -->
+			<footer id="footer"></footer>
+		<!-- Footer Section: End -->
+
+
+		<!-- JS files -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+	</body>
+</html>
